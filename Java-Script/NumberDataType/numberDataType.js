f7266e6fe285680bf53c0ee7b8976bf6c19434e6
sujanaankem <ankemsujana@gmail.com>
2026-06-05 16:03:45 +0530

    number data types

--- Java-Script/NumberDataType/numberDataType.js
+++ Java-Script/NumberDataType/numberDataType.js
@@ -2,35 +2,36 @@ alert("Are you sure you want to open this page");
 console.log("page opened");
 function onClickAddition(){
     debugger;
-    var firstName=Number(document.getElementById("data").value);
-    var secondName=Number(document.getElementById("table").value);
+    var firstName=Number(document.getElementById("num1").value);
+    var secondName=Number(document.getElementById("num2").value);
     var Addition = firstName + secondName;
     alert(Addition);
 }
 function onClickSubtract(){ 
     debugger;
-    var firstName=Number(document.getElementById("data").value);
-    var secondName=Number(document.getElementById("table").value);
+    var firstName=Number(document.getElementById("num1").value);
+    var secondName=Number(document.getElementById("num2").value);
     var sub = firstName - secondName;
     alert(sub);
 }
 function onClickMultiplication(){ 
     debugger;
-    var firstName=Number(document.getElementById("data").value);
-    var secondName=Number(document.getElementById("table").value);
+    var firstName=Number(document.getElementById("num1").value);
+    var secondName=Number(document.getElementById("num2").value);
     var mul = firstName * secondName;
     alert(mul);
 }
 function onClickDivision(){ 
     debugger;
-    var firstName=Number(document.getElementById("data").value);
-    var secondName=Number(document.getElementById("table").value);
+    var firstName=Number(document.getElementById("num1").value);
+    var secondName=Number(document.getElementById("num2").value);
     var div = firstName / secondName;
     alert(div);
 }
 function onClickRemainder(){
-    var firstName=Number(document.getElementById("data").value);
-    var secondName=Number(document.getElementById("table").value);
+    debugger;
+    var firstName=Number(document.getElementById("num1").value);
+    var secondName=Number(document.getElementById("num2").value);
     var remainder=firstName % secondName;
     alert(remainder);
 }
