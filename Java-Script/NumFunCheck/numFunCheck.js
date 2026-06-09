35ca960bb7a7a602892768d73e11cf9f0d5344c2
sujanaankem <ankemsujana@gmail.com>
2026-06-09 17:00:01 +0530

    Number function check

---
+++ Java-Script/NumFunCheck/numFunCheck.js
@@ -0,0 +1,7 @@
+ function onClickFunctionCheck(){
+               alert(Number.MIN_VALUE);
+               alert(Number.MAX_VALUE);
+               alert(Number.POSITIVE_INFINITY);
+               alert(Number.NEGATIVE_INFINITY);
+
+            }
