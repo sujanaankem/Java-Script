f73ee8ef6c57c729e476ee7efbde801bbce4a4ef
sujanaankem <ankemsujana@gmail.com>
2026-06-13 12:00:39 +0530

    ParseInt and ParseFloat

---
+++ Java-Script/Parseint/parseint.js
@@ -0,0 +1,10 @@
+function onClickCheck(){
+    debugger;
+    var number = document.getElementById("txtValue").value;
+    document.getElementById("pResult").innerHTML = parseInt(number);
+}
+function onClickCheckButton(){
+    debugger;
+    var number = document.getElementById("txtNumber").value;
+    document.getElementById("pResults").innerHTML = parseFloat(number);
+}
