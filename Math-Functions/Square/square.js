2ab3c0ade9e98f0b21e797536a4e10aed5febd8c
sujanaankem <ankemsujana@gmail.com>
2026-06-15 18:16:30 +0530

    Square root (Math function)

---
+++ Math-Functions/Square/square.js
@@ -0,0 +1,9 @@
+function onClickSquare(){
+    debugger;
+    let value = Number(document.getElementById("txtNumber").value);
+    document.getElementById("pResult").innerHTML = Math.sqrt(value);
+}
+function onClickStaticSquare(){
+    debugger;
+    document.getElementById("pValuet").innerHTML = Math.sqrt(64);
+}
